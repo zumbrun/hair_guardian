b6d49bd94af9edc5b148934d2087718599a09154
zumbrun <zumbrun@gmail.com>
2020-11-23 13:36:40 -0600

    added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
+	before_action :ensure_login
+	helper_method :logged_in?, :current_user
+
+	protected
+
+		def ensure_login
+			#always go to login page unless session contains uesr_id
+			redirect_to login_path unless session[:user_id]
+		end
+
+		def logged_in?
+			session[:user_id] # nil is false
+		end
+
+		def current_user
+			@current_user ||= User.find(session[:user_id])
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :caps do
   	resources :data, only: [:create, :destroy]
   end
 
   resources :data, only: [:index]
+  resources :sessions, only:[:new, :create, :destroy]
 
   root to: "caps#index"
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 
 end
