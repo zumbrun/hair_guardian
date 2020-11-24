2b5a993157fd1b1a810a1f5e520b1dd842a5234a
zumbrun <zumbrun@gmail.com>
2020-11-24 13:52:53 -0600

    updated database to fix error

--- db/migrate/20201110165620_create_users.rb
+++ db/migrate/20201110165620_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.text :password_digest
-      t.te :username
+      t.text :username
       t.integer :role
 
       t.timestamps
